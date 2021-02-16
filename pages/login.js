7bf7d9990d9dadce97d2c0a6cf0c0e97851b0f04
WPaczula <wojciech.paczula@gmail.com>
2021-02-16 15:55:51 +0100

    feat: Add validation

--- pages/login.js
+++ pages/login.js
@@ -11,7 +11,8 @@ import {
   InputGroup,
   InputRightElement,
   Button,
-  Icon,
+  FormErrorMessage,
+  CircularProgress,
 } from "@chakra-ui/react";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 
@@ -26,50 +27,100 @@ const validationSchema = yup.object().shape({
 
 const login = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
 
   return (
     <Flex width="full" align="center" justifyContent="center">
-      <Box p={8} mt={32} maxWidth="700px" borderRadius={8} boxShadow="lg">
+      <Box
+        p={8}
+        mt={32}
+        width="80%"
+        maxWidth="400px"
+        borderRadius={8}
+        boxShadow="lg"
+      >
         <Box textAlign="center">
           <Heading>Login</Heading>
         </Box>
         <Box my={4} textAlign="left">
           <Formik
-            onSubmit={() => {
-              alert("yup");
+            onSubmit={(values, { setSubmitting, validateForm }) => {
+              setSubmitted(false);
+              console.log("Hello");
+
+              validateForm()
+                .then((errors) => {
+                  if (!Object.values(errors).length) {
+                    setSubmitting(true);
+                    setTimeout(() => {
+                      setSubmitting(false);
+                      alert(JSON.stringify(values));
+                    }, 2000);
+                  }
+                })
+                .then(() => {
+                  setSubmitted(true);
+                });
             }}
             initialValues={{ email: "", password: "" }}
             validationSchema={validationSchema}
           >
-            <Form>
-              <FormControl>
-                <FormLabel>Email</FormLabel>
-                <Field id="emaiL" component={Input} type="email" name="email" />
-              </FormControl>
-              <FormControl mt={4}>
-                <FormLabel>Password</FormLabel>
-                <InputGroup>
-                  <Field
-                    id="password"
-                    component={Input}
-                    type={showPassword ? "text" : "password"}
-                    name="password"
-                  />
-                  <InputRightElement width="3rem">
-                    <Button
-                      h="1.5rem"
-                      size="sm"
-                      onClick={() => setShowPassword(!showPassword)}
-                    >
-                      {showPassword ? <ViewOffIcon /> : <ViewIcon />}
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-                <Button type="submit" width="100%" mt={10}>
-                  Log in
+            {({ isSubmitting }) => (
+              <Form noValidate>
+                <Field name="email">
+                  {({ field, meta }) => (
+                    <FormControl isInvalid={submitted && meta.error}>
+                      <FormLabel>Email</FormLabel>
+                      <Input id="emaiL" {...field} type="email" />
+                      <FormErrorMessage>{meta.error}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <Field name="password">
+                  {({ field, meta }) => (
+                    <FormControl mt={4} isInvalid={submitted && meta.error}>
+                      <FormLabel>Password</FormLabel>
+                      <InputGroup>
+                        <Input
+                          {...field}
+                          id="password"
+                          type={showPassword ? "text" : "password"}
+                          name="password"
+                        />
+                        <InputRightElement width="3rem">
+                          <Button
+                            h="1.5rem"
+                            size="sm"
+                            onClick={() => setShowPassword(!showPassword)}
+                          >
+                            {showPassword ? <ViewOffIcon /> : <ViewIcon />}
+                          </Button>
+                        </InputRightElement>
+                      </InputGroup>
+                      <FormErrorMessage>{meta.error}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <Button
+                  colorScheme="teal"
+                  type="submit"
+                  width="100%"
+                  mt={10}
+                  disabled={isSubmitting}
+                  onClick={() => setSubmitted(true)}
+                >
+                  {isSubmitting ? (
+                    <CircularProgress
+                      isIndeterminate
+                      size="24px"
+                      color="teal"
+                    />
+                  ) : (
+                    "Log in"
+                  )}
                 </Button>
-              </FormControl>
-            </Form>
+              </Form>
+            )}
           </Formik>
         </Box>
       </Box>
