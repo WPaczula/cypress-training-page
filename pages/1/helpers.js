4e1c0e74a8039e8ec1cd35497843c0f571fa1012
WPaczula <wojciech.paczula@gmail.com>
2021-02-21 15:40:53 +0100

    refactor: Move hints for first section

--- pages/1/helpers.js
+++ pages/1/helpers.js
@@ -11,7 +11,7 @@ const Helpers = () => {
         <Heading color="teal">Spis użytecznych komend</Heading>
         <Text mt={4} textAlign="justify">
           Jeżeli będziesz chciał sobie przypomnieć komendy lub sposób pisania
-          asercji/budowę testu odwiedź folder <Code>code</Code> w tej solucji.
+          asercji/budowę testu odwiedź folder <Code>help/1</Code> w tej solucji.
           Jeżeli czegoś w nim nie znajdziesz powiedz mi a ja postaram ci się
           pomóc i dodać to od razu dla potomnych 😄
         </Text>
