a59c3efd5503119afd02af1e60c87bf9082c7b8b
WPaczula <wojciech.paczula@gmail.com>
2021-03-03 21:31:23 +0100

    feat: Remove not used files andfill in README

--- cypress/integration/1/login.spec.js
+++
@@ -1,9 +0,0 @@
-describe("Login", () => {
-  it("should login the user if correct email and password are passed.", () => {
-    cy.visit("http://localhost:3000/login");
-
-    cy.login();
-
-    cy.location("pathname").should("be.eq", "/");
-  });
-});
